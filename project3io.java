e718cc8fa6aa15e397a84637f93c3710d20a3e53
Benjoe Gaburno <ggaburno@gmail.com>
2017-01-27 22:01:47 +0800

    pushed project3io.java

---
+++ project3io.java
@@ -0,0 +1,10 @@
+class project3io{
+    
+    public static void main(String [] args ){
+        for(String i : args ){
+            System.out.println(i);
+
+        }
+        System.out.println("there are " + args.length " arguments");
+    }
+}
